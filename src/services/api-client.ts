dcaee703b8b7f7ffa9f1ce507c7c3892d1d9201c
Murtaza Hassani <murtazahassani105@gmail.com>
2023-05-15 11:08:14 +0300

    first commit

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "64e51d10e7f84b2e9fea766fd510333d",
   },
 });
